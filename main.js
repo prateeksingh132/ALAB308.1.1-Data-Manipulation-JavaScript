6e2433bc29d97a145d86a02e283c11eb36fa0996
prateeksingh132 <kumarpks132@gmail.com>
2025-12-02 19:37:42 -0700

    Initial commit

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ const arithmetic2 = arithmatic1*n3;
 const remainder = arithmetic2%n4;
 console.log("Remainder: " + remainder);
 // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
-isOver25Part1 = (n1<=25)&&(n2<=25)&&(n3<=25)&&(n4<=25);
+const isOver25Part1 = (n1<=25)&&(n2<=25)&&(n3<=25)&&(n4<=25);
 console.log("Are all number below 25: " + isOver25Part1);
 
 
